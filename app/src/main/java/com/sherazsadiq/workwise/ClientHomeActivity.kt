be2655f9632406d57bbb8dbf66d5318628615188
msherazsadiq <sadiqmuhammadsheraz@gmail.com>
2024-04-15 15:07:59 +0500

    till place order screen

---
+++ app/src/main/java/com/sherazsadiq/workwise/ClientHomeActivity.kt
@@ -0,0 +1,21 @@
+package com.sherazsadiq.workwise
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ClientHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.client_home_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homescreen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
