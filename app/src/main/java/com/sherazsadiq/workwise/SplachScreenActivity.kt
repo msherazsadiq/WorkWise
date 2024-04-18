10014319be5385c0953b4d03f8f9c43252f06291
msherazsadiq <sadiqmuhammadsheraz@gmail.com>
2024-04-18 15:27:03 +0500

    till place order screen

--- app/src/main/java/com/sherazsadiq/workwise/SplachScreenActivity.kt
+++ app/src/main/java/com/sherazsadiq/workwise/SplachScreenActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ClientHomeActivity : AppCompatActivity() {
+class SplachScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.client_home_activity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homescreen)) { v, insets ->
+        setContentView(R.layout.activity_splach_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
