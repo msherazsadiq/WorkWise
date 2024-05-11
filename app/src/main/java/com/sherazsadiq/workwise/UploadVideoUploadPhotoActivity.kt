fee5c15ab97af98ff1541313942df6493031907b
msherazsadiq <sadiqmuhammadsheraz@gmail.com>
2024-05-11 19:55:57 +0500

    Added Add-Gig page

---
+++ app/src/main/java/com/sherazsadiq/workwise/UploadVideoUploadPhotoActivity.kt
@@ -0,0 +1,20 @@
+package com.sherazsadiq.workwise
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UploadVideoUploadPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_upload_video_upload_photo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
